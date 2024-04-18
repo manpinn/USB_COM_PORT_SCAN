a753803f63fa003fcfcba949c20c9089ebfee0c8
manpinn <mario.kozlovac@outlook.com>
2024-04-18 18:34:21 +0200

    Projektdateien hinzufügen.

---
+++ USB_COM_PORT_SCAN/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace USB_COM_PORT_SCAN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
